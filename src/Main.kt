ffc3470021921a6cbfc82f242daa7abbac420ce8
Brecht Loos <loos.brecht@gmail.com>
2023-12-04 22:22:41 +0100

    keep track of elapsed time, stil very basic

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@ import Day02.Day02
 import Day04.Day04
 import day03.Day03
 import day1.Day01
+import kotlin.system.measureTimeMillis
 
 
 fun main() {
@@ -20,7 +21,11 @@ fun solveDay(s: Solve) {
     s.solveTestInput()
     val input = readInput(s.getDay())
     println("--- Solution for ${s.getDay()} ---")
-    println("Part 1\t: ${s.solvePartOne(input)}")
-    println("Part 2\t: ${s.solvePartTwo(input)}")
-    println()
+    val elapsedPart1 = measureTimeMillis {
+        println("Part 1\t: ${s.solvePartOne(input)}")
+    }
+    val elapsedPart2 = measureTimeMillis {
+        println("Part 2\t: ${s.solvePartTwo(input)}")
+    }
+    println("Elapsed time in ms\n Part 1\t: ${elapsedPart1}\n  Part 2\t: ${elapsedPart2}\n")
 }
